117d829201c70d3cbfaedf4d7706030174d926bd
Mariya_L <mariya.lozhkina.n@gmail.com>
2021-10-27 16:24:31 +0300

    добавила Adapter

---
+++ app/src/main/java/com/mariyalozjkina/testappbnetkotlin/EntryAdapter.kt
@@ -0,0 +1,30 @@
+package com.mariyalozjkina.testappbnetkotlin
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class EntryAdapter(
+    private val selectEntryListener: SelectEntryListener,
+) : RecyclerView.Adapter<EntryListHolder>() {
+
+    private var items = listOf<Entry>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EntryListHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.item_entry_list, parent, false)
+        return EntryListHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: EntryListHolder, position: Int) {
+        holder.bind(items[position], selectEntryListener)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    fun setItems(items: List<Entry>) {
+        this.items = items
+    }
+}

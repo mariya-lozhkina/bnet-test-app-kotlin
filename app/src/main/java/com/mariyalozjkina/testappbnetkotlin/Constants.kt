6135d0ccbfb2eb81c7dd8c7728af1aa67b884fcb
Mariya_L <mariya.lozhkina.n@gmail.com>
2021-10-27 15:36:58 +0300

    добавила константы

---
+++ app/src/main/java/com/mariyalozjkina/testappbnetkotlin/Constants.kt
@@ -0,0 +1,11 @@
+package com.mariyalozjkina.testappbnetkotlin
+
+object Constants {
+    const val BODY = "body"
+    const val DA = "creationDate"
+    const val DM = "modificationDate"
+    const val ID = "id"
+    const val TOKEN = "0dB5ADp-8w-KDJ5c1G"
+    const val URL = "https://bnet.i-partner.ru/testAPI/"
+    const val SESSION = "session"
+}

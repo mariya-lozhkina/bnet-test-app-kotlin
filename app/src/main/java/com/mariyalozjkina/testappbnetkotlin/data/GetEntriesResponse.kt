d6ab786d2de4a1bd23297179bf7235465a69a223
Mariya_L <mariya.lozhkina.n@gmail.com>
2021-10-29 18:45:21 +0300

    заменила ArrayList на List

--- app/src/main/java/com/mariyalozjkina/testappbnetkotlin/data/GetEntriesResponse.kt
+++ app/src/main/java/com/mariyalozjkina/testappbnetkotlin/data/GetEntriesResponse.kt
@@ -1,8 +1,6 @@
 package com.mariyalozjkina.testappbnetkotlin.data
 
-import java.util.*
-
 data class GetEntriesResponse(
     val status: Int,
-    val data: ArrayList<ArrayList<Entry>>?
+    val data: List<List<Entry>>?
 )
